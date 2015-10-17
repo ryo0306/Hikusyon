bb74da34aa1ca49a2ace33e80378e791e03cb240
ryo0306 <iroha_0416@yahoo.co.jp>
2015-10-17 18:35:55 +0900

    ブラッシュアップ（途中）

--- 9月審査会ブラッシュアップ/src/Collision.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include "ScreenMake.h"
-
-bool BoxCollision(Vec2f pos1_, Vec2f size1_, Vec2f pos2_, Vec2f size2_);
-
-bool CircleCollision(Vec2f pos1_, Vec2f size1_, Vec2f pos2_, Vec2f size2_);
